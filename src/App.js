00aaa8fa30729052afc47522856a8a03886c7b3f
Mario Calderón <mariocldrn@gmail.com>
2018-09-18 18:25:18 -0600

    cambio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 import {animes} from './animes.json';
 console.log(animes)
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      animes
+    }
+  }
+
   render() {
+    console.log('Antes de renderizar el componente');
     return (
       <div className = "App">
         <nav className = "navbar navbar-dark bg-dark">
