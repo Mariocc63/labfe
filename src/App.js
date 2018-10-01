5d166eb244a8ed1aade943e00c7f7424877e47d5
Mario Calderón <mariocldrn@gmail.com>
2018-09-30 20:46:39 -0600

    cambio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-
 import Redirect from './Components/Redirect';
 
 //console.log({animes})
 class App extends React.Component {
 
   render() {
-  
-
     return (
-      
-
             <div>
               <Redirect></Redirect>
             </div>
-
-
     );
   }
 }
